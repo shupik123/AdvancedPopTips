aedf6a6c91352db096d2742445dc31e79d26a43e
shupik <edo4eddie@gmail.com>
2021-04-08 15:34:45 -0400

    sync before readme

--- Main.cs
+++ Main.cs
@@ -20,8 +20,8 @@ namespace AdvancedPopTips
         //private static System.Collections.Generic.Dictionary<string, string> LocaleOverrides =
         //    new System.Collections.Generic.Dictionary<string, string>()
         //    {
-        //        {"SniperMonkey Description", "Monkey shoots big dumb bloon hah."},
-        //        {"SuperMonkey Description", "I'm like super man but my laser eyes are actually trash."}
+        //        {"SniperMonkey Description", "no shot"},
+        //        {"SuperMonkey Description", "i dart monke but zoooooom"}
         //    };
 
         [HarmonyPatch]
@@ -44,35 +44,44 @@ namespace AdvancedPopTips
                     yield return new WaitForEndOfFrame();
                 }
 
-                string[] fileEntries = Directory.GetFiles("Mods/AdvancedPopTips/"); // getting files from directory
 
-                foreach (var file in fileEntries)
+                if (!Directory.Exists("Mods/AdvancedPopTips/")) // downloading the overrides
                 {
-                    if (file.Substring(file.Length - 4, 4).Equals(".txt"))
-                    {
-                        string rawText = File.ReadAllText(file);
-                        string[] overrides = rawText.Split('\n');
-
-                        foreach (var edit in overrides) // editing text table entries
-                        {
-                            string[] parts = edit.Split('|');
-
-                            LocalizationManager.instance.textTable[parts[0]] = parts[1];
-                        }
-                    }
-
+                    // download and unzip
                 }
 
+                string[] fileEntries = Directory.GetFiles("Mods/AdvancedPopTips/"); // getting files from directory
+                foreach (var file in fileEntries)
+                {
+                    if (file.Substring(file.Length - 4, 4).Equals(".txt")) // check file extension
+                    {
+                        string rawText = File.ReadAllText(file);
+                        string[] overrides = rawText.Split('\n'); // split file into lines
+
+                        foreach (var edit in overrides) // for every line
+                        {
+                            string[] parts = edit.Split('|'); // parsing the mod's file
+
+                            LocalizationManager.instance.textTable[parts[0]] = parts[1]; // editing text table entry
+                        }
+                    }
+                }
+
+
+                //    // writes all the text table data - to be commented out for normal mod builds
                 
-                // for listing all the text table values
-                //foreach (var entry in LocalizationManager.instance.textTable)
-                //{
-                //    using (System.IO.StreamWriter file =
-                //        new System.IO.StreamWriter(@"BTD6TextTable.txt", true))
+                //    foreach (var entry in LocalizationManager.instance.textTable) // for listing all the text table values
                 //    {
-                //        file.WriteLine("L: " + LocalizationManager.instance.activeLanguage + " || K:" + entry.key + " || V:" + entry.value);
+                //        using (System.IO.StreamWriter file =
+                //            new System.IO.StreamWriter(@"BTD6TextTable.txt", true))
+                //        {
+                //            // only one of these should be uncommented
+
+                //            file.WriteLine("L: " + LocalizationManager.instance.activeLanguage + " || K:" + entry.key + " || V:" + entry.value);
+                //            //file.WriteLine(entry.key + "|");
+                //        }
                 //    }
-                //}
+
             }
 
             static void Postfix()
